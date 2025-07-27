eb231fbed451a5d3781e25817dd19667dd1c9817
ADMINSTRATOR KRISHNA <krishnakumarshresthaa@gmail.com>
2025-07-27 20:22:38 +0545

    Configured Database

--- Expense_Track/settings.py
+++ Expense_Track/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'Expense_Track.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Expense_Track_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
